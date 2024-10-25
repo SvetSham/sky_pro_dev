eec3451eb308262671d30549bb33624e1b875129
Svetlana Shambina <szxd2009@yandex.ru>
2024-10-25 16:15:59 +0300

    Упрощение функции test_get_mask_card_number_normal

--- tests/test_masks.py
+++ tests/test_masks.py
@@ -3,9 +3,8 @@ import pytest
 from src.masks import get_mask_account, get_mask_card_number
 
 
-@pytest.mark.parametrize("input_int, output_str", [(1596837868705199, "1596 83** **** 5199")])
-def test_get_mask_card_number_normal(input_int: int, output_str: str) -> None:
-    assert get_mask_card_number(input_int) == output_str
+def test_get_mask_card_number_normal() -> None:
+    assert get_mask_card_number(1596837868705199) == "1596 83** **** 5199"
 
 
 def test_get_mask_card_number_short() -> None:
