8b2982311b89f56c74ae6be2c24f8300d634a849
Svetlana Shambina <szxd2009@yandex.ru>
2025-04-09 15:22:21 +0300

    Добавление логирования в модуль utils.py

--- src/utils.py
+++ src/utils.py
@@ -1,4 +1,18 @@
 import json
+import logging
+import os
+
+if os.path.basename(os.getcwd()) == "HomeWork_9.1":
+    filename = "logs/utils.log"
+else:
+    filename = "../logs/utils.log"
+
+logger = logging.getLogger("utils")
+logger.setLevel(logging.DEBUG)
+file_handler = logging.FileHandler(filename, encoding="utf-8")
+file_formatter = logging.Formatter("%(asctime)s - %(name)s - %(levelname)s: %(message)s")
+file_handler.setFormatter(file_formatter)
+logger.addHandler(file_handler)
 
 
 def read_json_file(path_to_file: str) -> list:
@@ -8,17 +22,26 @@ def read_json_file(path_to_file: str) -> list:
     try:
         with open(path_to_file, "rt", encoding="utf-8") as file:
             content = file.read()
-    except FileNotFoundError:
+    except FileNotFoundError as fnf:
+        logger.error(f"Произошла ошибка: {fnf}")
         return []
     else:
+        logger.info("Файл открыт успешно")
         if content:
+            logger.info("Файл прочитан")
             try:
                 transactions = json.loads(content)
-            except json.decoder.JSONDecodeError:
+            except json.decoder.JSONDecodeError as jse:
+                logger.error(f"Произошла ошибка: {jse}")
                 return []
+            else:
+                logger.info("Десериализация прошла успешно")
         else:
+            logger.error("Файл пуст")
             return []
         if isinstance(transactions, list):
+            logger.info("Получен список")
             return transactions
         else:
+            logger.error("Получен не список")
             return []
