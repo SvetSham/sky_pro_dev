2f0896a7df7daf05bf7b8e7ce94bfca856a6ebd5
Svetlana Shambina <szxd2009@yandex.ru>
2025-06-03 18:02:17 +0300

    Создание модуля чтения csv и xslx файлов

--- src/read_csv_xslx.py
+++ src/read_csv_xslx.py
@@ -1,1 +1,10 @@
 import pandas as pd
+
+def read_transactions_csv(path_to_csv_file: str) -> list:
+    transactions = pd.read_csv(path_to_csv_file, delimiter=';')
+    return transactions.to_dict(orient='records')
+
+
+def read_transactions_xslx(path_to_xlsx_file: str) -> list:
+    transactions = pd.read_excel(path_to_xlsx_file)
+    return transactions.to_dict(orient='records')
