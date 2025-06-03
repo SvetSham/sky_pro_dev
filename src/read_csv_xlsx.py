7d20d84a61f1f9a376337bfc14b377eb4f097c06
Svetlana Shambina <szxd2009@yandex.ru>
2025-06-03 20:26:24 +0300

    Оформление кода и добавление docstrings в read_csv_xlsx.py

--- src/read_csv_xlsx.py
+++ src/read_csv_xlsx.py
@@ -2,10 +2,14 @@ import pandas as pd
 
 
 def read_transactions_csv(path_to_csv_file: str) -> list[dict]:
+    """ Функция принимает на вход путь к csv-файлу с финансовыми транзакциями
+     и считывает его в список словарей """
     transactions = pd.read_csv(path_to_csv_file, delimiter=";")
     return transactions.to_dict(orient="records")
 
 
 def read_transactions_xlsx(path_to_xlsx_file: str) -> list[dict]:
+    """ Функция принимает на вход путь к xlsx-файлу с финансовыми транзакциями
+         и считывает его в список словарей """
     transactions = pd.read_excel(path_to_xlsx_file)
     return transactions.to_dict(orient="records")
