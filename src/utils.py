bb904e1b57ee904c03bc90d5b0787fb44ae2b770
Svetlana Shambina <szxd2009@yandex.ru>
2025-06-12 13:43:01 +0300

    Перенос функций process_bank_search и process_bank_operations из utils.py в processing.py

--- src/utils.py
+++ src/utils.py
@@ -1,8 +1,7 @@
 import json
 import logging
 import os
-import re
-from collections import Counter
+
 
 if os.path.basename(os.getcwd()) == "HomeWork_9.1":
     filename = "logs/utils.log"
@@ -47,23 +46,3 @@ def read_json_file(path_to_file: str) -> list:
         else:
             logger.error("Получен не список")
             return []
-
-
-def process_bank_search(data: list[dict], str_for_search: str) -> list[dict]:
-    """Функция принимает список словарей с данными о банковских операциях и строку поиска,
-    и возвращает список словарей, у которых в описании есть данная строка."""
-    found_data = []
-    for transaction in data:
-        if re.search(str_for_search, transaction["description"], flags=re.IGNORECASE):
-            found_data.append(transaction)
-    return found_data
-
-
-def process_bank_operations(data: list[dict], categories: list) -> dict:
-    """Функция принимает список словарей с данными о банковских операциях и список категорий операций,
-    и возвращает словарь, в котором ключи — это названия категорий, а значения — это количество операций в каждой категории."""
-    operations_in_categories = []
-    for transaction in data:
-        operations_in_categories.append(transaction["description"])
-    num_operations_in_categories = Counter(operations_in_categories)
-    return dict(num_operations_in_categories)
