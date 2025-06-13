11619b0afadb4f8a756df23f6766e2eb2baf6c3d
Svetlana Shambina <szxd2009@yandex.ru>
2025-06-13 11:17:55 +0300

    Модификация функции mask_account_card, чтобы card_number был типа str

--- src/widget.py
+++ src/widget.py
@@ -19,11 +19,11 @@ def mask_account_card(account_card: str) -> str:
     else:
         if len(description_account_card) < 2:
             raise ValueError("Вы не ввели номер карты.")
-        if description_account_card[-1].isalpha():
+        if not description_account_card[-1].isdigit():
             raise ValueError("Вы не ввели номер карты.")
-        card_number: int
+        card_number: str
         try:
-            card_number = int(description_account_card[-1])
+            card_number = description_account_card[-1]
         except ValueError:
             raise ValueError("Номер карты должен содержать только цифры.")
         result = get_mask_card_number(card_number)
