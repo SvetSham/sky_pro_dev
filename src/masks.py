83d1ae48b24fb190bf4503d4dbf402ca7970f6a7
Svetlana Shambina <szxd2009@yandex.ru>
2025-06-13 12:15:56 +0300

    Приведение кода в соответствие к требованиям PEP8

--- src/masks.py
+++ src/masks.py
@@ -14,9 +14,9 @@ file_handler.setFormatter(file_formatter)
 logger.addHandler(file_handler)
 
 
-def get_mask_card_number(card_number: str = '-1') -> str:
+def get_mask_card_number(card_number: str = "-1") -> str:
     """Функция маскировки номера банковской карты"""
-    if card_number == '-1':
+    if card_number == "-1":
         logger.error("Номер карты не введён")
         raise ValueError("Вы не ввели номер карты.")
     if not card_number.isdigit():
@@ -44,20 +44,20 @@ def get_mask_card_number(card_number: str = '-1') -> str:
     return mask_card_number
 
 
-def get_mask_account(account_number: str = '-1') -> str:
+def get_mask_account(account_number: str = "-1") -> str:
     """Функция маскировки номера банковского счета"""
-    if account_number == '-1':
-        logger.error('Номер счёта не введён')
+    if account_number == "-1":
+        logger.error("Номер счёта не введён")
         raise ValueError("Вы не ввели номер счёта.")
     if not account_number.isdigit():
-        logger.error('Номер счёта - не число')
+        logger.error("Номер счёта - не число")
         raise ValueError("Номер счёта должен содержать только цифры.")
     if len(account_number) < 20:
-        logger.error('Номер счёта короче 20 цифр')
+        logger.error("Номер счёта короче 20 цифр")
         raise ValueError("Номер счёта слишком короткий. Должно быть 20 цифр.")
     elif len(account_number) > 20:
-        logger.error('Номер счёта длиннее 20 цифр')
+        logger.error("Номер счёта длиннее 20 цифр")
         raise ValueError("Номер счёта слишком длинный. Должно быть 20 цифр.")
     mask_account = "**" + account_number[-4:]
-    logger.info('Номер счёта замаскирован')
+    logger.info("Номер счёта замаскирован")
     return mask_account
--- src/processing.py
+++ src/processing.py
@@ -1,6 +1,7 @@
 import re
 from collections import Counter
 
+
 def filter_by_state(transaction: list[dict], state: str = "EXECUTED") -> list[dict]:
     """Функция фильтрует транзакции по состоянию"""
     filtered_by_state = []
@@ -28,7 +29,9 @@ def process_bank_search(data: list[dict], str_for_search: str) -> list[dict]:
 
 def process_bank_operations(data: list[dict], categories: list) -> dict:
     """Функция принимает список словарей с данными о банковских операциях и список категорий операций,
-    и возвращает словарь, в котором ключи — это названия категорий, а значения — это количество операций в каждой категории."""
+    и возвращает словарь, в котором ключи — это названия категорий,
+    а значения — это количество операций в каждой категории.
+    """
     operations_in_categories = []
     for transaction in data:
         operations_in_categories.append(transaction["description"])
